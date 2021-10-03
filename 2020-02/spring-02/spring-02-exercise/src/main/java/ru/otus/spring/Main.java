4b79d85311561f1e30a0056f834a4372acb1f1ca
artym <artymk77@gmail.com>
2021-10-03 13:10:32 +0300

    delete package Config and add @Service and Component

--- 2020-02/spring-02/spring-02-exercise/src/main/java/ru/otus/spring/Main.java
+++ 2020-02/spring-02/spring-02-exercise/src/main/java/ru/otus/spring/Main.java
@@ -3,10 +3,13 @@ package ru.otus.spring;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Service;
 import ru.otus.spring.domain.Person;
 import ru.otus.spring.service.PersonService;
-@ComponentScan
+
+
 @Configuration
+@ComponentScan
 public class Main {
 
     public static void main(String[] args) {
--- 2020-02/spring-02/spring-02-exercise/src/main/java/ru/otus/spring/service/PersonServiceImpl.java
+++ 2020-02/spring-02/spring-02-exercise/src/main/java/ru/otus/spring/service/PersonServiceImpl.java
@@ -1,8 +1,13 @@
 package ru.otus.spring.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import ru.otus.spring.dao.PersonDao;
 import ru.otus.spring.domain.Person;
 
+@Service
 public class PersonServiceImpl implements PersonService {
 
     private final PersonDao dao;
