759850781feb5547bcb7073b20ffb042a2e77d1e
artym <artymk77@gmail.com>
2021-10-03 22:55:54 +0300

    add Aspect in log to observe starting class

--- 2020-02/spring-03-aop/spring-03-aop-classwork/spring-03-exercise/src/main/java/ru/otus/spring/logging/LoggingAspect.java
+++ 2020-02/spring-03-aop/spring-03-aop-classwork/spring-03-exercise/src/main/java/ru/otus/spring/logging/LoggingAspect.java
@@ -1,9 +1,17 @@
 package ru.otus.spring.logging;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
 
+@Aspect
+@Component
+@Order(10)
 public class LoggingAspect {
 
+    @Before("execution(* ru.otus.spring.dao.*.*(String))")
     public void logBefore(JoinPoint joinPoint) {
         System.out.println("Прокси : " + joinPoint.getThis().getClass().getName());
         System.out.println("Класс : " + joinPoint.getTarget().getClass().getName());
